c2dc50c80de41c0a7a3fc38eddfb8e5e33944a6c
Ubuntu <kunaya-code@kunaya-IT-lab.tj1lwydduwve5ljur3t5pgkase.rx.internal.cloudapp.net>
2026-04-17 11:18:38 +0000

    feat: vercel proxy rewrites to azure vm

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: '/n8n/:path*',
+        destination: 'http://40.81.230.135/n8n/:path*', // Azure VM IP
+      },
+      {
+        source: '/sherlock/:path*',
+        destination: 'http://40.81.230.135/sherlock/:path*', // Azure VM IP
+      }
+    ];
+  },
 };
 
 export default nextConfig;
